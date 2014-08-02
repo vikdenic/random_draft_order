87fc7219c9a4f1758364461b678f2f148469439d
vikdenic <vik.denic@gmail.com>
2014-08-02 16:54:43 -0500

    Initial Commit

---
+++ Bernie_Bucket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bernie_Bucket
+//
+//  Created by Vik Denic on 8/2/14.
+//  Copyright (c) 2014 Vik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
