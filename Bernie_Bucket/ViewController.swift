057adab74d48d36749b138ca80c51339d8653b88
vikdenic <vik.denic@gmail.com>
2014-08-02 20:02:28 -0500

    logic and animations implemented

--- Bernie_Bucket/ViewController.swift
+++ Bernie_Bucket/ViewController.swift
@@ -27,11 +27,12 @@ class ViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
+        refillArray()
     }
 
     func refillArray()
     {
-        ownersArray = ["Vik", "Hal", "Dale", "Jim", "Topel", "Stein", "Jon", "D'Amico", "Craig", "Oso", "Ross", "Reddy"]
+        ownersArray = ["Vik", "Hal", "Dale", "Jim", "Ropes", "Styne", "Jon", "D'Amico", "Craig", "Oso", "Ross", "Yam"]
     }
 
     @IBAction func onRandomButtonTapped(sender: UIButton)
@@ -102,12 +103,137 @@ class ViewController: UIViewController {
         refillArray()
     }
 
+    @IBAction func onTwelveTapped(sender: UITapGestureRecognizer)
+    {
+        var pickTwelve = Int(arc4random_uniform(12))
+        animateLabel(twelveLabel, delay: 5)
+        twelveLabel.text = "12: " + ownersArray[pickTwelve]
+        println(ownersArray)
+        ownersArray.removeAtIndex(pickTwelve)
+    }
+
+    @IBAction func onElevenTapped(sender: UITapGestureRecognizer)
+    {
+        var pickEleven = Int(arc4random_uniform(11))
+        animateLabel(elevenLabel, delay: 5)
+        elevenLabel.text = "11: " + ownersArray[pickEleven]
+        println(ownersArray)
+        ownersArray.removeAtIndex(pickEleven)
+    }
+
+    @IBAction func onTenTapped(sender: UITapGestureRecognizer)
+    {
+        var pickTen = Int(arc4random_uniform(10))
+        animateLabel(tenLabel, delay: 5)
+        tenLabel.text = "10: " + ownersArray[pickTen]
+        println(ownersArray)
+        ownersArray.removeAtIndex(pickTen)
+    }
+
+    @IBAction func onNineTapped(sender: UITapGestureRecognizer)
+    {
+        var pickNine = Int(arc4random_uniform(9))
+        animateLabel(nineLabel, delay: 5)
+        nineLabel.text = "9: " + ownersArray[pickNine]
+        println(ownersArray)
+        ownersArray.removeAtIndex(pickNine)
+    }
+
+    @IBAction func onEightTapped(sender: UITapGestureRecognizer)
+    {
+        var pickEight = Int(arc4random_uniform(8))
+        animateLabel(eightLabel, delay: 5)
+        eightLabel.text = "8: " + ownersArray[pickEight]
+        println(ownersArray)
+        ownersArray.removeAtIndex(pickEight)
+    }
+
+    @IBAction func onSevenTapped(sender: UITapGestureRecognizer)
+    {
+        var pickSeven = Int(arc4random_uniform(7))
+        animateLabel(sevenLabel, delay: 5)
+        sevenLabel.text = "7: " + ownersArray[pickSeven]
+        println(ownersArray)
+        ownersArray.removeAtIndex(pickSeven)
+    }
+
+    @IBAction func onSixTapped(sender: UITapGestureRecognizer)
+    {
+        var pickSix = Int(arc4random_uniform(6))
+        animateLabel(sixLabel, delay: 5)
+        sixLabel.text = "6: " + ownersArray[pickSix]
+        println(ownersArray)
+        ownersArray.removeAtIndex(pickSix)
+    }
+
+    @IBAction func onFiveTapped(sender: UITapGestureRecognizer)
+    {
+        var pickFive = Int(arc4random_uniform(5))
+        animateLabel(fiveLabel, delay: 5)
+        fiveLabel.text = "5: " + ownersArray[pickFive]
+        println(ownersArray)
+        ownersArray.removeAtIndex(pickFive)
+    }
+
+    @IBAction func onFourTapped(sender: UITapGestureRecognizer)
+    {
+        var pickFour = Int(arc4random_uniform(4))
+        animateLabel(fourLabel, delay: 5)
+        fourLabel.text = "4: " + ownersArray[pickFour]
+        println(ownersArray)
+        ownersArray.removeAtIndex(pickFour)
+    }
+
+    @IBAction func onThreeTapped(sender: UITapGestureRecognizer)
+    {
+        var pickThree = Int(arc4random_uniform(3))
+        animateLabel(threeLabel, delay: 5)
+        threeLabel.text = "3: " + ownersArray[pickThree]
+        println(ownersArray)
+        ownersArray.removeAtIndex(pickThree)
+    }
+
+    @IBAction func onTwoTapped(sender: UITapGestureRecognizer)
+    {
+        var pickTwo = Int(arc4random_uniform(2))
+        animateLabel(twoLabel, delay: 5)
+        twoLabel.text = "2: " + ownersArray[pickTwo]
+        println(ownersArray)
+        ownersArray.removeAtIndex(pickTwo)
+    }
+
+    @IBAction func onOneTapped(sender: UITapGestureRecognizer)
+    {
+        var pickOne = Int(arc4random_uniform(1))
+        animateLabel(oneLabel, delay: 5)
+        oneLabel.text = "1: " + ownersArray[pickOne]
+        println(ownersArray)
+        ownersArray.removeAtIndex(pickOne)
+        println(ownersArray)
+    }
+
+    @IBAction func clearBoard(sender: UIButton)
+    {
+        refillArray()
+        oneLabel.text = "1"
+        twoLabel.text = "2"
+        threeLabel.text = "3"
+        fourLabel.text = "4"
+        fiveLabel.text = "5"
+        sixLabel.text = "6"
+        sevenLabel.text = "7"
+        eightLabel.text = "8"
+        nineLabel.text = "9"
+        tenLabel.text = "10"
+        elevenLabel.text = "11"
+        twelveLabel.text = "12"
+    }
+
     func animateLabel(label : UILabel, delay : NSNumber)
     {
         label.alpha = 0
         UIView.animateWithDuration(delay, animations: {
             label.alpha = 1
-            })
+        })
     }
 }
-
